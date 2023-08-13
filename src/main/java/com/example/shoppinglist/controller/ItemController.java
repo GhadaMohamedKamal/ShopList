2a938a6363a18e07f590e6ccccd53037e5bcba55
Ghada Mohamed <98233671+GhadaMohamedKamal@users.noreply.github.com>
2023-08-13 17:38:17 +0200

    updateItemController.java

--- src/main/java/com/example/shoppinglist/controller/ItemController.java
+++ src/main/java/com/example/shoppinglist/controller/ItemController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bosch.ordermanager.modell.Candidate;
+
 import com.example.shoppinglist.model.Item;
 import com.example.shoppinglist.service.ItemService;
 
