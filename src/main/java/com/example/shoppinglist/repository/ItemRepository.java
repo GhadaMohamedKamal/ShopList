6b34945007e2d8ecd960f56a27bce5e7d9f9ad89
Ghada Mohamed <98233671+GhadaMohamedKamal@users.noreply.github.com>
2023-08-13 17:11:27 +0200

    Update ItemRepository.java

--- src/main/java/com/example/shoppinglist/repository/ItemRepository.java
+++ src/main/java/com/example/shoppinglist/repository/ItemRepository.java
@@ -1,10 +1,19 @@
 package com.example.shoppinglist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.shoppinglist.model.Item;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-}
+	@Transactional   
+	void deleteItemById(Long id);
+
+        Optional<Item> getItemById(Long id);
+			
+		}
+
